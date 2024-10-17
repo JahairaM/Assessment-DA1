8b0fa8f79b63b326158f0200d4c73007264204ce
Jahaira Maxwell-Myers <119506562+JahairaM@users.noreply.github.com>
2024-10-16 21:41:22 -0400

    Add files via upload

--- da-assessment-1-JahairaM-main/part2-answers.sql
+++ da-assessment-1-JahairaM-main/part2-answers.sql
@@ -1,10 +1,12 @@
 -- Paste your answers for each question in the appropriate spot below. (Keep the number headings)
 -- #1
-
-
-
-
+SELECT *
+FROM students
+WHERE firstname LIKE 'J%' AND id > 3;
 
 -- #2
-
+SELECT s.firstname, s.lastname
+FROM students s
+JOIN assignments a ON s.id = a.student_id
+WHERE a.title = 'Geography Quiz' AND a.score > 90;
 
