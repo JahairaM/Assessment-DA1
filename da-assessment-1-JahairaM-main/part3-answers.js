99af22653028b745617385d1b3799ecedb5700fd
Jahaira Maxwell-Myers <119506562+JahairaM@users.noreply.github.com>
2024-10-16 21:41:51 -0400

    Add files via upload

--- da-assessment-1-JahairaM-main/part3-answers.js
+++ da-assessment-1-JahairaM-main/part3-answers.js
@@ -1,11 +1,18 @@
 // Paste your answers for each question in the appropriate spot below. (Keep the number headings)
 // #1
 
-
-
-
+const shows = db.shows.find(
+    {
+        $or: [
+            { artist: "The Supremes" },
+            { artist: "The Temptations" }
+        ]
+    }
+);
 
 // #2
-
-
-
+shows.forEach(show => {
+    if (show.artist === "Aretha Franklin" && show.floor === "Main Floor") {
+        printjson(show.number_sold);
+    }
+});
